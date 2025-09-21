270b22c55bc20af6497ab4cb74e61ef21e640183
Rootless <clementdud57@gmail.com>
2025-09-21 19:00:56 +0200

    Create GUIExample.java

---
+++ GUIExample.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUIExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simple GUI Example");
+        JButton button = new JButton("Click Me");
+
+        button.setBounds(100, 100, 150, 50);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Button Clicked!");
+            }
+        });
+
+        frame.add(button);
+        frame.setSize(400, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
